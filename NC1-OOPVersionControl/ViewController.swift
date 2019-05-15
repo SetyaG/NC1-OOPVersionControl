7e88a8d59335afbc2552a77fdab61fedfee2fbc6
SetyaG <owen.pg27@gmail.com>
2019-05-15 08:14:27 +0700

    Initial Commit

---
+++ NC1-OOPVersionControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NC1-OOPVersionControl
+//
+//  Created by Owen Prasetya on 15/05/19.
+//  Copyright © 2019 Owen Prasetya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
